e55f5e4919e64fb3240af75ccba048a06181f577
melih <melihayhan@gmail.com>
2023-12-06 07:37:37 +0300

    first commit

---
+++ src/AuctionService/DTOs/UpdateAuctionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace AuctionService.DTOs;
+
+public class UpdateAuctionDto
+{
+    // [Required] 
+    public string Make { get; set; }
+    // [Required] 
+    public string Model { get; set; }
+    // [Required] 
+    public int? Year { get; set; }
+    // [Required] 
+    public string Color { get; set; }
+    // [Required] 
+    public int? Mileage { get; set; }
+}
