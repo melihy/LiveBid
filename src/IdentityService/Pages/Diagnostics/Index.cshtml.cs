8dc8f8bc4eb4bed9f2fe445ade073687504597bf
melih <melihayhan@gmail.com>
2024-01-01 23:17:48 +0300

    Dockerizing services..

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -13,7 +13,13 @@ public class Index : PageModel
         
     public async Task<IActionResult> OnGet()
     {
-        var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
+        var localAddresses = new string[] { 
+            "::ffff:192.168.65.1", 
+            "127.0.0.1", 
+            "::1", 
+            HttpContext.Connection.LocalIpAddress.ToString() 
+            };
+            
         if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
         {
             return NotFound();
